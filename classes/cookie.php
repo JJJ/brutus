03afae31b96fd5e57bad0665a095dbd87b419de6
John James Jacoby <johnjamesjacoby@me.com>
2015-04-13 17:39:45 -0500

    Whitespace reset to cookie.php

--- classes/cookie.php
+++ classes/cookie.php
@@ -19,77 +19,77 @@ defined( 'ABSPATH' ) || exit;
  */
 final class Brutus_Cookie {
 
-    /**
-     * @var string The name of our cookie
-     */
-    protected $cookie_name;
+	/**
+	 * @var string The name of our cookie
+	 */
+	protected $cookie_name;
 
-    /**
-     * @var int When the cookie expires
-     */
-    protected $expires;
+	/**
+	 * @var int When the cookie expires
+	 */
+	protected $expires;
 
-    /**
-     * @var boolean Whether or not to set an SSL only cookie
-     */
-    protected $secure;
+	/**
+	 * @var boolean Whether or not to set an SSL only cookie
+	 */
+	protected $secure;
 
-    /**
-     * @var string Nonce salt we'll use to "sign" our IDs
-     */
-    protected $salt;
+	/**
+	 * @var string Nonce salt we'll use to "sign" our IDs
+	 */
+	protected $salt;
 
-    /**
-     * @var PasswordHash Container for an instance of PasswordHash
-     */
-    protected $hasher = null;
+	/**
+	 * @var PasswordHash Container for an instance of PasswordHash
+	 */
+	protected $hasher = null;
 
-    /**
-     * Constructor
-     *
-     * @since   Brutus (1.0.0)
-     *
-     * @param   string  $cookie_name
-     * @param   int     $expires
-     * @param   boolean $secure
-     * @param   string  $salt
-     */
-    public function __construct( $cookie_name = '', $expires = '', $secure = false, $salt = '' ) {
-        $this->cookie_name = $cookie_name;
-        $this->expires     = $expires;
-        $this->secure      = $secure;
-        $this->salt        = $salt;
-    }
+	/**
+	 * Constructor
+	 *
+	 * @since   Brutus (1.0.0)
+	 *
+	 * @param   string  $cookie_name
+	 * @param   int     $expires
+	 * @param   boolean $secure
+	 * @param   string  $salt
+	 */
+	public function __construct( $cookie_name = '', $expires = '', $secure = false, $salt = '' ) {
+		$this->cookie_name = $cookie_name;
+		$this->expires     = $expires;
+		$this->secure      = $secure;
+		$this->salt        = $salt;
+	}
 
-    /**
+	/**
 	 * Maybe set a cookie with a unique user ID to use for nonces
 	 *
-     * @since Brutus (1.0.0)
-     */
-    public function init() {
-        list( $uid, $expires, $sig ) = $this->cookie_id();
+	 * @since Brutus (1.0.0)
+	 */
+	public function init() {
+		list( $uid, $expires, $sig ) = $this->cookie_id();
 
-        // if we already have an ID set up, don't bother
-        if ( ! empty( $uid ) && $this->valid_expiration( $expires ) && $this->valid_signature( $uid, $sig ) ) {
-            return;
-        }
+		// if we already have an ID set up, don't bother
+		if ( ! empty( $uid ) && $this->valid_expiration( $expires ) && $this->valid_signature( $uid, $sig ) ) {
+			return;
+		}
 
-        $user_id = md5( $this->get_hasher()->get_random_bytes( 64 ) );
+		$user_id = md5( $this->get_hasher()->get_random_bytes( 64 ) );
 
-        $this->set_cookie( $user_id );
-    }
+		$this->set_cookie( $user_id );
+	}
 
-    /**
+	/**
 	 * Get the unique user ID
 	 *
 	 * Used by `nonce_user_logged_out` filter
 	 *
-     * @since Brutus (1.0.0)
-     */
-    public function get_user_id() {
-        list( $uid, $expires, $sig ) = $this->cookie_id();
-        return $uid;
-    }
+	 * @since Brutus (1.0.0)
+	 */
+	public function get_user_id() {
+		list( $uid, $expires, $sig ) = $this->cookie_id();
+		return $uid;
+	}
 
 	/**
 	 * Kill the nonce cookie when user logs in
@@ -117,122 +117,122 @@ final class Brutus_Cookie {
 
 	/** Protected Methods *****************************************************/
 
-    /**
-     * Get the user ID from the cookie.
-     *
-     * @since   Brutus (1.0.0)
-     *
-     * @return  array [$uid, $expires] or [null, null] on failure
-     */
-    protected function cookie_id() {
+	/**
+	 * Get the user ID from the cookie.
+	 *
+	 * @since   Brutus (1.0.0)
+	 *
+	 * @return  array [$uid, $expires] or [null, null] on failure
+	 */
+	protected function cookie_id() {
 
 		// Bail with empty array
-        if ( ! isset( $_COOKIE[ $this->cookie_name ] ) ) {
-            return array( null, null, null );
-        }
+		if ( ! isset( $_COOKIE[ $this->cookie_name ] ) ) {
+			return array( null, null, null );
+		}
 
-        $cookie = $_COOKIE[ $this->cookie_name ];
+		$cookie = $_COOKIE[ $this->cookie_name ];
 
 		// Bail with empty array
-        if ( 2 !== substr_count( $cookie, '|' ) ) {
-            return array( null, null, null );
-        }
+		if ( 2 !== substr_count( $cookie, '|' ) ) {
+			return array( null, null, null );
+		}
 
-        return explode( '|', $cookie );
-    }
+		return explode( '|', $cookie );
+	}
 
-    /**
-     * Set the nonce cookie.
-     *
-     * @since   Brutus (1.0.0)
-     *
-     * @param   string $uid The user ID to set
-     * @return  void
-     */
-    protected function set_cookie( $uid ) {
+	/**
+	 * Set the nonce cookie.
+	 *
+	 * @since   Brutus (1.0.0)
+	 *
+	 * @param   string $uid The user ID to set
+	 * @return  void
+	 */
+	protected function set_cookie( $uid ) {
 
 		// Setup cookie values
-        $expires = time() + $this->expires;
-        $value   = $uid . '|' . $expires . '|' . $this->sign( $uid );
+		$expires = time() + $this->expires;
+		$value   = $uid . '|' . $expires . '|' . $this->sign( $uid );
 
-        // Put the user ID into the $_COOKIE superglobal
-        $_COOKIE[ $this->cookie_name ] = $value;
+		// Put the user ID into the $_COOKIE superglobal
+		$_COOKIE[ $this->cookie_name ] = $value;
 
 		// Drop a delicious cookie
-        return setcookie(
-            $this->cookie_name,
-            $value,
-            $expires,
-            COOKIEPATH,
-            COOKIE_DOMAIN,
-            $this->secure
-        );
-    }
+		return setcookie(
+			$this->cookie_name,
+			$value,
+			$expires,
+			COOKIEPATH,
+			COOKIE_DOMAIN,
+			$this->secure
+		);
+	}
 
-    /**
-     * Get the hasher, used so we can generate a nice set of random bytes
-     *
-     * @since   Brutus (1.0.0)
+	/**
+	 * Get the hasher, used so we can generate a nice set of random bytes
 	 *
-     * @return  PasswordHash
-     */
-    protected function get_hasher() {
+	 * @since   Brutus (1.0.0)
+	 *
+	 * @return  PasswordHash
+	 */
+	protected function get_hasher() {
 
 		// Return hasher if already set
-        if ( ! is_null( $this->hasher ) ) {
-            return $this->hasher;
-        }
+		if ( ! is_null( $this->hasher ) ) {
+			return $this->hasher;
+		}
 
 		// Require phpass
-        require_once ABSPATH . 'wp-includes/class-phpass.php';
+		require_once ABSPATH . 'wp-includes/class-phpass.php';
 
 		// Setup the hasher
-        $this->hasher = new PasswordHash( 8, true );
+		$this->hasher = new PasswordHash( 8, true );
 
-        return $this->hasher;
-    }
+		return $this->hasher;
+	}
 
-    /**
-     * Check to see if an expires time falls without our allowed time limits.
-     *
-     * @since   Brutus (1.0.0)
+	/**
+	 * Check to see if an expires time falls without our allowed time limits.
 	 *
-     * @param   int $expires The unix timestamp to check
-     * @return  boolean
-     */
-    protected function valid_expiration( $expires ) {
-        $diff = intval( $expires ) - time();
-
-        // If we've passed our day threshold return false
-        if ( $diff <= DAY_IN_SECONDS ) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Check a signature and see if it's valid.
-     *
-     * @since   Brutus (1.0.0)
+	 * @since   Brutus (1.0.0)
 	 *
-     * @param   string $uid
-     * @param   string $sig
-     * @return  boolean
-     */
-    protected function valid_signature( $uid, $sig ) {
-        return 0 === strcmp( $sig, $this->sign( $uid ) );
-    }
+	 * @param   int $expires The unix timestamp to check
+	 * @return  boolean
+	 */
+	protected function valid_expiration( $expires ) {
+		$diff = intval( $expires ) - time();
 
-    /**
-     * Sign $uid with our salt.
-     *
-     * @since   Brutus (1.0.0)
+		// If we've passed our day threshold return false
+		if ( $diff <= DAY_IN_SECONDS ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Check a signature and see if it's valid.
 	 *
-     * @param   string $uid
-     * @return  string
-     */
-    protected function sign( $uid ) {
-        return hash_hmac( 'sha1', $uid, $this->salt );
-    }
+	 * @since   Brutus (1.0.0)
+	 *
+	 * @param   string $uid
+	 * @param   string $sig
+	 * @return  boolean
+	 */
+	protected function valid_signature( $uid, $sig ) {
+		return 0 === strcmp( $sig, $this->sign( $uid ) );
+	}
+
+	/**
+	 * Sign $uid with our salt.
+	 *
+	 * @since   Brutus (1.0.0)
+	 *
+	 * @param   string $uid
+	 * @return  string
+	 */
+	protected function sign( $uid ) {
+		return hash_hmac( 'sha1', $uid, $this->salt );
+	}
 }
